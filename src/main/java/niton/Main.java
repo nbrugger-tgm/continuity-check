5388e43aaab54d14acd0941fb713453844849817
Nils Brugger <nbrugger@student.tgm.ac.at>
2021-02-13 14:45:46 +0100

    feat: initial code

---
+++ src/main/java/niton/Main.java
@@ -0,0 +1,124 @@
+package niton;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+	public static void main(String[] args) throws IOException, ImageProcessingException {
+
+		Scanner s = new Scanner(System.in);
+		System.out.print("Folder to scan > ");
+		File folder = new File(s.nextLine());
+		System.out.print("Visit subfolders (y/n) >");
+		boolean subs = s.nextLine().equalsIgnoreCase("y");
+		System.out.print("Use indentation (y/n) >");
+		boolean indent = s.nextLine().equalsIgnoreCase("y");
+
+		File[] folders;
+		if (subs) {
+			folders = folder.listFiles(f -> !f.isFile());
+		} else {
+			folders = new File[]{folder};
+		}
+		Arrays.parallelSort(folders, Comparator.comparing(File::getAbsolutePath));
+		for (File file : folders) {
+			System.out.println("\nAnalysis > " + file);
+			File[] files = file.listFiles(
+					f -> (f.isFile() &&
+							(f.getAbsolutePath()
+							  .toLowerCase()
+							  .endsWith(".jpg") ||
+									f.getAbsolutePath()
+									 .toLowerCase()
+									 .endsWith(".jpeg") ||
+									f.getAbsolutePath()
+									 .toLowerCase()
+									 .endsWith(".png")))
+			);
+			System.out.println("Found " + files.length + " images");
+			SortedMap<LocalDate, List<File>> map = new TreeMap<>();
+			System.out.print("Scan : ");
+			for (int i = 0; i < files.length; i++) {
+				File f = files[i];
+				System.out.print("\rScan : " + f.getName());
+				try {
+					Metadata metadata = ImageMetadataReader.readMetadata(f);
+					ExifSubIFDDirectory directory = metadata.getFirstDirectoryOfType(
+							ExifSubIFDDirectory.class);
+					long timeInMs = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL)
+					                         .getTime();
+					LocalDate key = Instant.ofEpochMilli(timeInMs)
+					                       .atZone(ZoneId.systemDefault())
+					                       .toLocalDate();
+					if (!map.containsKey(key)) {
+						map.put(key, new ArrayList<>());
+					}
+					map.get(key).add(f);
+				} catch (Exception e) {
+					System.out.println("Error on this file(" + f + ") : " + e);
+				}
+			}
+			DateTimeFormatter form = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+			for (Map.Entry<LocalDate, List<File>> day : map.entrySet()) {
+				try {
+					day.getValue().sort(Comparator.comparingLong(Main::getFileCreationEpoch));
+					System.out.println("\n"+form.format(day.getKey()) + " : ");
+					System.out.println((indent ? "\t" : "") + "Files : " + day.getValue().size());
+					File first = day.getValue().get(0);
+					System.out.println((indent ? "\t" : "") + "First : " + first.getName());
+					File last = day.getValue().get(day.getValue().size() - 1);
+					System.out.println((indent ? "\t" : "") + "Last  : " + last.getName());
+					int start = parse(first), end = parse(last);
+					System.out.println((indent ? "\t" : "") + "Missing: " + (((end - start) + 1) - day
+							.getValue()
+							.size()));
+					List<Integer> numbs = day.getValue()
+					                        .stream()
+					                        .map(Main::parse)
+					                        .collect(Collectors.toList());
+					if (indent) {
+						for (int i = start; i < end; i++) {
+							if (!numbs.contains(i)) {
+								System.out.println("\t\t> " + i);
+							}
+						}
+					} else {
+						System.out.println(IntStream.range(start,end).filter((i)->!numbs.contains(i)).mapToObj(String::valueOf).collect(Collectors.joining(", ")));
+					}
+				} catch (Exception e) {
+					System.out.println("Failure interpreting a file from " + day.getKey());
+				}
+			}
+		}
+
+	}
+
+	public static long getFileCreationEpoch(File file) {
+		try {
+			Metadata            metadata  = ImageMetadataReader.readMetadata(file);
+			ExifSubIFDDirectory directory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+			Date                date      = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+			return date.getTime();
+		} catch (IOException | ImageProcessingException e) {
+			throw new RuntimeException(file.getAbsolutePath(), e);
+		}
+	}
+
+	public static int parse(File f) {
+		int firstPoint = f.getName().indexOf(".");
+		return Integer.parseInt(f.getName().substring(firstPoint - 4, firstPoint));
+	}
+}
